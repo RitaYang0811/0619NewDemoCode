96720ccbd013f7c731be9580de3124e2c979b7cb
RitaYang <ritayang0811@gmail.com>
2025-06-19 21:54:47 +0800

    test lint

--- src/sum.test.js
+++ src/sum.test.js
@@ -5,6 +5,6 @@ describe("sum", () => {
     expect(sum(1, 2)).toBe(3);
   });
   it("should return 0.3 when 0.1+0.2", () => {
-       expect(sum(0.1, 0.2)).toBe(0.3);
+    expect(sum(0.1, 0.2)).toBe(0.3);
   });
 });
