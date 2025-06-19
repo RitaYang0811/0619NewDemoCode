00d1dabe1f5c7fe11bfdbaf8dfbdb894dfd77a8b
RitaYang <ritayang0811@gmail.com>
2025-06-19 22:14:42 +0800

    test

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,9 +1,9 @@
-const isEven = require('./utils');
+const isEven = require("./utils");
 
-test('returns true for even number', () => {
+test("returns true for even number", () => {
   expect(isEven(4)).toBe(true);
 });
 
-test('returns false for odd number', () => {
+test("returns false for odd number", () => {
   expect(isEven(5)).toBe(false);
 });
